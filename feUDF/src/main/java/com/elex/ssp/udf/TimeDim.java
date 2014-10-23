aeda5946a571ab9b98def8f5333f47791f46f4d5
wuzhongju <wuzhongju@126.com>
2014-10-23 15:59:04 +0800

    update

--- feUDF/src/main/java/com/elex/ssp/udf/TimeDim.java
+++ feUDF/src/main/java/com/elex/ssp/udf/TimeDim.java
@@ -16,41 +16,41 @@ import com.elex.ssp.TimeUtils;
 
 public class TimeDim extends GenericUDTF {
 	
-	String hour;
-	String workOrVaction;
-	String dayPart;
+	Object hour[] = new Object[1];
+	Object workOrVaction[] = new Object[1];
+	Object dayPart[] = new Object[1];
+	
+	@Override
+	public void close() throws HiveException {
 
-	 
-	  @Override
-	  public void close() throws HiveException {
+	}
 
-	  }
-	 
-	  @Override
-	  public StructObjectInspector initialize(ObjectInspector[] argOIs) throws UDFArgumentException {
-	    ArrayList<String> fieldNames = new ArrayList<String>();
-	    ArrayList<ObjectInspector> fieldOIs = new ArrayList<ObjectInspector>();
-	    fieldNames.add("col1");
-	    fieldOIs.add(PrimitiveObjectInspectorFactory.javaStringObjectInspector);
-	    return ObjectInspectorFactory.getStandardStructObjectInspector(fieldNames,
-	fieldOIs);
-	  }
-	 
-	  @Override
-	  public void process(Object[] args) throws HiveException {
-		  String[] myArgs = {args[0].toString(),args[1].toString()};
-		 try {
+	@Override
+	public StructObjectInspector initialize(ObjectInspector[] argOIs)throws UDFArgumentException {
+		ArrayList<String> fieldNames = new ArrayList<String>();
+		ArrayList<ObjectInspector> fieldOIs = new ArrayList<ObjectInspector>();
+		fieldNames.add("col1");
+		fieldOIs.add(PrimitiveObjectInspectorFactory.javaStringObjectInspector);
+		return ObjectInspectorFactory.getStandardStructObjectInspector(fieldNames, fieldOIs);
+	}
+
+	@Override
+	public void process(Object[] args) throws HiveException {
+		
+		
+		String[] myArgs = { args[0].toString(), args[1].toString() };
+		try {
 			Date day = TimeUtils.getTimeByNation(myArgs);
-			hour = TimeUtils.getHour(day);
-			dayPart = TimeUtils.getDayPart(day);
-			workOrVaction = TimeUtils.isWorkOrVacation(day);
-			forward((Object)hour);
-			forward((Object)dayPart);
-			forward((Object)workOrVaction);
-			
+			hour[0] = TimeUtils.getHour(day);
+			dayPart[0] = TimeUtils.getDayPart(day);
+			workOrVaction[0] = TimeUtils.isWorkOrVacation(day);
+			forward(hour);
+			forward(dayPart);
+			forward(workOrVaction);
+
 		} catch (ParseException e) {
 			e.printStackTrace();
 		}
-	  }
+	}
 
 }
